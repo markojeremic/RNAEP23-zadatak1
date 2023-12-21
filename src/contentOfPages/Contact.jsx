934eddd33539e453c3404d0b2319a622418d58ea
jeremicmarko <jeremic.z.marko@gmail.com>
2023-12-21 19:08:22 +0100

    initial commit

---
+++ src/contentOfPages/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Networks from '../components/Networks';
+function Contact() {
+  return (
+    <div className="condiv">
+            <h1 className="subtopic">Contact Us</h1>
+            <h3>Email  :   antonije990099@gmail.com</h3>
+            <h3>Instagram   :   @antonijedimitrijevic99</h3>
+            <Networks />
+
+            <h3>Email  :   marko.jeremicsd@gmail.com</h3>
+            <h3>Instagram   :   @jeramarkovic</h3>
+            <Networks />
+            </div>
+  )
+}
+
+export default Contact
